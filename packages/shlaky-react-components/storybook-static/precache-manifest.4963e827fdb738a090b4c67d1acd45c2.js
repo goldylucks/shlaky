51f7402650d67a25f6e03ac787762d9cf50b3dee
goldy <goldy@adamgoldman.me>
2019-10-31 15:29:21 +0200

    rename shlaky-components to shlaky-react-components

---
+++ packages/shlaky-react-components/storybook-static/precache-manifest.4963e827fdb738a090b4c67d1acd45c2.js
@@ -0,0 +1,15 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ad1c2e0c553a78a62e7318268102cde",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.661143185427ef3fe1ca.bundle.js"
+  },
+  {
+    "url": "runtime~main.661143185427ef3fe1ca.bundle.js"
+  },
+  {
+    "url": "vendors~main.661143185427ef3fe1ca.bundle.js"
+  }
+]);
