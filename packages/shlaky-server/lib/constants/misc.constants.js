da82d89e890ac222ca5c4f315fc714dcab424d32
goldy <goldy@adamgoldman.me>
2019-11-02 08:40:42 +0200

    update git ignore

--- packages/shlaky-server/lib/constants/misc.constants.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = void 0;
-const miscConstants = {};
-var _default = miscConstants;
-exports.default = _default;
