f7a1423ef28766f32957d0b00a0ba34217435e3d
goldy <goldy@adamgoldman.me>
2019-11-04 11:24:00 +0200

    move example to examples folder

--- example/server/dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-require("source-map-support/register");
-
-var _server = _interopRequireDefault(require("./server"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-(0, _server.default)();
